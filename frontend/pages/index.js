106891502431d8113d160c1eb5df497b0b9316a3
valuedriven <junilson@hotmail.com>
2022-02-22 18:40:08 -0300

    Configuração inicial

---
+++ frontend/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Cloud Native App</title>
+        <meta name="description" content="Example of Cloud Native App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p />
+        <h1>Welcome to DevOps-cyo App!</h1>
+      </main>
+    </div>
+  );
+}
+
